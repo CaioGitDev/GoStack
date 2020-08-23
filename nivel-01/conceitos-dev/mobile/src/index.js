217db9dc5de95e5daebf03e8f82eac220b36ffb2
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-08-23 22:12:50 +0100

    feat: get api projects

--- nivel-01/conceitos-dev/mobile/src/index.js
+++ nivel-01/conceitos-dev/mobile/src/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import api from './services/api'
 
 // não possuem valor semântico
 // não possuem estilização própria
@@ -8,6 +9,18 @@ import { View, Text, StyleSheet, StatusBar } from 'react-native'
 //Text: p span strong h1 h2 h3
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    console.log('response.data');
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data);
+    }).catch(error => {
+      console.log(error);
+    });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
