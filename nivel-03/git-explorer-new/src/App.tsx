6bf3ffee5676542de2befd154d40779103aa2034
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-09-09 22:28:53 +0100

    feat: add global styles and svg background

--- nivel-03/git-explorer-new/src/App.tsx
+++ nivel-03/git-explorer-new/src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
 const App: React.FC = () => {
-  return <Routes />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
