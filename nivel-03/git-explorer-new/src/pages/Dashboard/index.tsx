1e24aaa9a534ba66d311eff4bed76aad1e807535
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-09-19 23:08:40 +0100

    feat: add repositories to local storaged

--- nivel-03/git-explorer-new/src/pages/Dashboard/index.tsx
+++ nivel-03/git-explorer-new/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import {
@@ -22,8 +22,22 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storegedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
 
+    if (storegedRepositories) {
+      return JSON.parse(storegedRepositories);
+    } else return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> {
